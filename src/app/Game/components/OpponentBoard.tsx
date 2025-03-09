1f32eb0a3d46548ae2b936890d4adb693aac9b9c
Luiz Henrique Ferreira <luizhff@gmail.com>
2025-03-09 17:18:12 -0300

    feat: create cardSlot hover effect

--- src/app/Game/components/OpponentBoard.tsx
+++ src/app/Game/components/OpponentBoard.tsx
@@ -2,11 +2,11 @@ export const OpponentBoard = () => {
   return (
     <div className="flex flex-col flex-1 gap-1">
       <div className="flex flex-1 justify-center">
-        <div className="border rounded h-full aspect-square"></div>
+        <div className="border rounded h-28 w-28"></div>
       </div>
       <div className="flex flex-1 justify-center gap-1">
-        <div className="border rounded h-full aspect-square"></div>
-        <div className="border rounded h-full aspect-square"></div>
+        <div className="border rounded h-28 w-28"></div>
+        <div className="border rounded h-28 w-28"></div>
       </div>
     </div>
   )
