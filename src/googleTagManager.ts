90e7c12b10499ac2cdcd014e4a2f1085d53da381
Luiz Henrique Ferreira <luizhff@gmail.com>
2025-03-24 11:20:24 -0300

    fix: correct google tag manager ID usage in tracking script

--- src/googleTagManager.ts
+++ src/googleTagManager.ts
@@ -3,7 +3,7 @@ export function googleTagManager() {
   console.log('tracking', !!import.meta.env.VITE_GOOGLE_TAG_MANAGER_ID);
   if (import.meta.env.VITE_GOOGLE_TAG_MANAGER_ID) {
     const script = document.createElement('script');
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${import.meta.env.VITE_GOOGLE_ANALYTICS_ID}`;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${import.meta.env.VITE_GOOGLE_TAG_MANAGER_ID}`;
     script.async = true;
     document.body.appendChild(script);
     script.onload = () => {
@@ -11,7 +11,7 @@ export function googleTagManager() {
       window.dataLayer = window.dataLayer || [];
       function gtag() { window.dataLayer.push(arguments) }
       gtag('js', new Date());
-      gtag('config', import.meta.env.VITE_GOOGLE_ANALYTICS_ID);
+      gtag('config', import.meta.env.VITE_GOOGLE_TAG_MANAGER_ID);
     };
   }
 }
