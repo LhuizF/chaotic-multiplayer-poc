359eb5f5ff3554ddcf6c1401df94c98171df9c1c
Luiz Henrique Ferreira <luizhff@gmail.com>
2025-03-24 22:02:08 -0300

    update images

--- src/cards/attacks.ts
+++ src/cards/attacks.ts
@@ -18,65 +18,52 @@ export const attacks: Attack[] = [
   },
   {
     id: '2',
-    name: 'Meteoro 2',
+    name: 'Chicote de Fogo',
     damage: 50,
-    image: '/src/assets/attacks/meteor.png',
+    image: '/src/assets/attacks/whip_fire.jpg',
     element: 'Fire'
   },
   {
     id: '3',
-    name: 'Meteoro 3',
+    name: 'Lança de Gelo',
     damage: 50,
-    image: '/src/assets/attacks/meteor.png',
-    element: 'Fire'
+    image: '/src/assets/attacks/ice_spear.jpg',
+    element: 'Water'
   },
   {
     id: '4',
-    name: 'Meteoro 4',
+    name: 'Jato de Água',
     damage: 50,
-    image: '/src/assets/attacks/meteor.png',
-    element: 'Fire'
+    image: '/src/assets/attacks/water_jet.png',
+    element: 'Water'
   },
   {
     id: '5',
-    name: 'Meteoro 5',
+    name: 'Onda de Pedra',
     damage: 50,
-    image: '/src/assets/attacks/meteor.png',
-    element: 'Fire'
+    image: '/src/assets/attacks/stone_wave.jpg',
+    element: 'Earth'
   },
   {
     id: '6',
-    name: 'Meteoro 6',
+    name: 'Coluna de Pedra',
     damage: 50,
-    image: '/src/assets/attacks/meteor.png',
-    element: 'Fire'
+    image: '/src/assets/attacks/rock_column.jpg',
+    element: 'Earth'
   },
   {
     id: '7',
-    name: 'Meteoro 7',
+    name: '',
     damage: 50,
-    image: '/src/assets/attacks/meteor.png',
-    element: 'Fire'
+    image: '/src/assets/attacks/',
+    element: ''
   },
   {
     id: '8',
-    name: 'Meteoro 8',
+    name: 'Flecha de Luz',
     damage: 50,
-    image: '/src/assets/attacks/meteor.png',
-    element: 'Fire'
+    image: '/src/assets/attacks/arrow_of_light.jpg',
+    element: 'Air'
   },
-  {
-    id: '9',
-    name: 'Meteoro 9',
-    damage: 50,
-    image: '/src/assets/attacks/meteor.png',
-    element: 'Fire'
-  },
-  {
-    id: '10',
-    name: 'Meteoro 10',
-    damage: 50,
-    image: '/src/assets/attacks/meteor.png',
-    element: 'Fire'
-  }
+
 ]
